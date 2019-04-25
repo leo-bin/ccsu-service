3e3877216301dbb3925640553ed3cb5c12ad2999
hangs.zhang <1078504296@qq.com>
2019-04-26 00:00:29 +0800

    first commit

---
+++ src/test/java/com/hang/CcsuServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hang;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CcsuServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
